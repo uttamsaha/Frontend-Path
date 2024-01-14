394e83cc3bdc7edbe4a742c315337864eed993dc
uttamsaha <uttamsaha982@gmail.com>
2024-01-15 01:33:57 +0600

    upload code

---
+++ Module 1/1-module/src/app.js
@@ -0,0 +1,35 @@
+// console.log("Hello")
+
+import { AnotherHello, HelloWorld } from "./another";
+import React from "react";
+import ReactDOM from 'react-dom/client'
+const App = () => {
+
+    return React.createElement(
+      "h1",
+      { style: { color: "red" } },
+      "This is h1 tag new",
+      HelloWorld(),
+      AnotherHello()
+    );
+  
+};
+const root = ReactDOM.createRoot(document.getElementById("root")); //creating root
+
+
+root.render(React.createElement(App)); //rendering app
+
+
+/*
+
+// const HelloWorld = () => {
+//     return <div>Hello world</div> 
+// }
+// const App = () => {
+//     return (<React.Fragment><div>This is a manual react app</div> <HelloWorld/></React.Fragment>)
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
+
+*/
