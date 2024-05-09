85448fa7dbdb187d9ec5e316abe3a3c706fac46c
Uttam Kumar Saha <uttamsaha982@live.com>
2024-05-10 01:13:30 +0600

    cng

--- Module 1/4-react-reuseable-component/src/pages/Home.tsx
+++ Module 1/4-react-reuseable-component/src/pages/Home.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <button className='lg:btn-solid'>This is home btn</button>
+    </div>
   )
 }
 
--- Module 1/4-react-reuseable-component/src/utils/cn.tsx
+++ Module 1/4-react-reuseable-component/src/utils/cn.tsx
@@ -1,7 +1,8 @@
 import { twMerge } from "tailwind-merge";
 import { ClassValue, clsx } from "clsx";
 const cn = (...inputs: ClassValue[]) => {
-    return twMerge(clsx(inputs));
+  //...inputs is a params being spread.
+  return twMerge(clsx(inputs));
 };
 
 export default cn;
