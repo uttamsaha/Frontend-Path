2f0e5198d94b9dfa360291e0f520960775f960d9
Uttam Kumar Saha <uttamsaha982@live.com>
2024-05-13 00:09:28 +0600

    reusable form

--- Module 1/4-react-reuseable-component/src/pages/Home.tsx
+++ Module 1/4-react-reuseable-component/src/pages/Home.tsx
@@ -1,12 +1,40 @@
 import React from "react";
 import NormalForm from "../components/NormalForm/NormalForm";
 import CommonForm from "../components/NormalForm/CommonForm";
+import {
+  Form,
+  FormSection,
+  FormSubmit,
+  Input,
+} from "../components/ReuseableForm";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Home = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
   return (
     <div>
       <NormalForm />
-      <CommonForm/>
+      <CommonForm />
+
+{/* <h2>Reusabel Form</h2> */}
+      <Form double={false} onSubmit={handleSubmit(onSubmit)}>
+        <FormSection>
+          <Input
+            type="email"
+            register={register("email")}
+            errors={errors}
+            label="Email"
+          />
+        </FormSection>
+        <FormSubmit />
+      </Form>
     </div>
   );
 };
