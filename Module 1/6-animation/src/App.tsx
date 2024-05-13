68c274cf6c720124d2a097b030c58a5c24fa185c
Uttam Kumar Saha <uttamsaha982@live.com>
2024-05-14 01:16:46 +0600

    animation

--- Module 1/6-animation/src/App.tsx
+++ Module 1/6-animation/src/App.tsx
@@ -4,7 +4,6 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-
   ///Chek app.css file
   return (
     <>
@@ -16,6 +15,29 @@ function App() {
 
       <div className="btn-container">
         <button className="btn">Hover me</button>
+      </div>
+
+      <div className="wrapper">
+        <div className="content"></div>
+      </div>
+
+      <div className="loading-container">
+        <div className="loading">
+          <div className="glass"></div>
+          <div className="loading-content"></div>
+        </div>
+      </div>
+
+      <div className="card-wrapper">
+        <div className="card">
+          <h1>Heading</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+            excepturi, suscipit ratione temporibus possimus nulla. Culpa in
+            commodi optio sed ipsum molestias et rem? Vel cumque culpa deserunt
+            pariatur sed?
+          </p>
+        </div>
       </div>
     </>
   );
